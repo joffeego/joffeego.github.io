532c1f26292bccde3908d3b9b1278a9b37a6d755
joffeego <joffeego@users.noreply.github.com>
2026-07-30 23:31:32 +0800

    🔍 添加全文搜索功能（Pagefind）

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import pagefind from 'astro-pagefind';
 
 export default defineConfig({
   site: 'https://joffee.eu.org',
-  integrations: [sitemap()],
+  integrations: [sitemap(), pagefind()],
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'hover',
